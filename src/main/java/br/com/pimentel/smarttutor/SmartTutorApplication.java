e7aae8f1f9fd5eed1c64ef28f2f89636940bfb70
Rodrigo Pimentel <pimentelmsn@hotmail.com>
2018-12-18 22:14:45 -0300

    Initial commit

---
+++ src/main/java/br/com/pimentel/smarttutor/SmartTutorApplication.java
@@ -0,0 +1,14 @@
+package br.com.pimentel.smarttutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTutorApplication.class, args);
+	}
+
+}
+
